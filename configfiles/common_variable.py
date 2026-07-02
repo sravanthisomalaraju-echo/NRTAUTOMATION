f6b56e38993cd1e0c2e5b9af3bd2318b4f5251ca
Sravanthi Somalaraju <sravanthi.somalaraju@dish.com>
2026-07-02 11:37:28 -0600

    new yml and config files
    Added new yml to run nightly cron job and updated common_variables

--- configfiles/common_variable.py
+++ configfiles/common_variable.py
@@ -1,6 +1,12 @@
+import os
+
+# os.environ.get() looks for the environment variable first (from GitHub Actions).
+# If it doesn't find it, it falls back to the second value (for your local computer).
+
 LOGIN_CONFIG = {
     'baseurl' : 'https://dish-nventory-temp.10xpeople.com/#/login', #test-setup
-    'defaultloginusername' : 'Sravanthi.Somalaraju@dish.com' ,
-    'defaultloginpassword' : 'Dish123!' ,
+    'defaultloginusername': os.environ.get('PORTAL_USER', 'Sravanthi.Somalaraju@dish.com'),
+    'defaultloginpassword': os.environ.get('PORTAL_PASS', 'Dish123!')
 }
 
+# ... any other variables you already have in this file ...
