098a70d2710e75886df33f506b16613adb8683b2
Sravanthi Somalaraju <sravanthi.somalaraju@dish.com>
2026-07-14 16:23:41 -0600

    updated webdriverfactory.py

--- base/webdriverfactory.py
+++ base/webdriverfactory.py
@@ -1,7 +1,5 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import os
 os.environ['WDM_SSL_VERIFY'] = '0'
 from configfiles.common_variable import *
